836dff696802c944f22ef38cb3ca928fc94a6de7
Dan Hellem <dan_hellem@hotmail.com>
2022-08-30 06:47:37 -0700

    commented out unused files

--- AdminService/Misc/AuthorizationRequirement.cs
+++ AdminService/Misc/AuthorizationRequirement.cs
@@ -1,16 +1,16 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using Microsoft.AspNetCore.Authorization;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace AdminService.Misc
-{
-    public class AuthorizationRequirement : IAuthorizationRequirement
-    {       
-        public AuthorizationRequirement()
-        {
+//namespace AdminService.Misc
+//{
+//    public class AuthorizationRequirement : IAuthorizationRequirement
+//    {       
+//        public AuthorizationRequirement()
+//        {
             
-        }
-    }
-}
+//        }
+//    }
+//}
